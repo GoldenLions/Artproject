a62242b6dbeee67ca90a3d831e7191e3e14f0750
Chun Hao Wang <chunhao137@gmail.com>
2014-09-10 12:00:03 -0700

    Added recommendation view

--- client/www/scripts/app.js
+++ client/www/scripts/app.js
@@ -3,15 +3,28 @@
 /*
  * Main module of the application.
  */
-angular
-  .module('dangerousWrenchApp', [
-    'ngAnimate',
+
+ angular.module('dangerousWrenchApp', ['ionic','ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
   ])
+
+ .run(function($ionicPlatform) {
+   $ionicPlatform.ready(function() {
+     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+     // for form inputs)
+     if(window.cordova && window.cordova.plugins.Keyboard) {
+       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+     }
+     if(window.StatusBar) {
+       StatusBar.styleDefault();
+     }
+   });
+ })
+
   .config(function ($locationProvider, $routeProvider) {
     $locationProvider
     $routeProvider
@@ -34,6 +47,10 @@ angular
       .when('/homepage/:user', {
         templateUrl: 'views/userPageView.html',
         controller: 'UserController'
+      })
+      .when('/recommendation', {
+        templateUrl: 'views/recommendation.html',
+        controller: 'UserRecommend'
       })
       .otherwise({
         redirectTo: '/'
---
+++ client/www/scripts/controllers/artRecommend.js
@@ -0,0 +1,18 @@
+angular.module('dangerousWrenchApp')
+
+.controller('UserRecommend', function ($scope) {
+ 
+    $scope.items = [
+    { id: 1, album: 'Lady and the Ermin', artist: 'Leonardo Da Vinci', image:"http://upload.wikimedia.org/wikipedia/commons/e/ed/Dama_z_gronostajem.jpg"},
+    { id: 2, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
+    { id: 3, album: 'Bacchus and Ariadne', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Tizian_048_%28colors%29.jpg/640px-Tizian_048_%28colors%29.jpg"},
+    { id: 4, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
+    { id: 5, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
+    { id: 6, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
+    { id: 7, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
+    { id: 8, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"}
+
+  ]
+  
+    
+})
