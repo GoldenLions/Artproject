f324a928798d3caa02152e9e7121d4c54e2d8941
wykhuh <wykhuh@users.noreply.github.com>
2014-09-11 15:35:30 -0700

    (feat/Recommendations) add Like function

--- client/www/scripts/services/likebuttonService.js
+++ client/www/scripts/services/likebuttonService.js
@@ -1,11 +1,26 @@
 angular.module('dangerousWrenchApp')
-.factory('likeButton', function($http){
+.factory('likeButton', function($http, $q){
 
-  var like = function(id){
-    return $http.get('/like/'+id);
-  }
+  var like = function(likeObject){
+    var deferred = $q.defer();
+    var httpPromise = $http({
+      method: 'POST',
+      url: 'like',
+      data: likeObject
+    });
+
+    httpPromise.then(function(response){
+      deferred.resolve(response);
+    }, function(error) {
+      console.log('like error', error);
+    });
+
+    return deferred.promise;
+
+  };
 
   return {
     like: like
-  }
-})
+  };
+});
+
--- server/routes.js
+++ server/routes.js
@@ -80,7 +80,7 @@ module.exports = function(app){
     })
   })
 
-  //keyword search needs s3
+  // Searched title and artist name for a keyword
   app.post('/KeywordSearch', function(req, res) {
     var searchterms = req.body.searchterms;
     var searchterms = searchterms.split(' ');
@@ -113,15 +113,23 @@ module.exports = function(app){
     })
   })
 
-  app.get('/like/:id', function(req, res){
-    var params = { id: parseInt(req.params.id), user: parseInt(req.user.id) };
+  // when user clicks like, incretment the like
+  app.post('/like/:id', function(req, res){
+    // var params = { id: parseInt(req.params.id), user: parseInt(req.user.id) };
+
+    var params = { id: req.params.url, user: req.user.username };
+
+
     db.query('MATCH (n:User),(b:Work)\nWHERE id(n)=({user}) AND id(b)=({id})\nCREATE (n)-[:LIKES {rating:1}]->(b)', params, function(err){
       if (err) console.log(err);
       console.log('like created!');
       console.log(req.user);
       console.log(params);
       res.end();
+      console.log(res.end())
     })
   })
+
+
   
 };
