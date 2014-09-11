160b7a638162ce09e722ebff42afd91a7a1875ce
Chun Hao Wang <chunhao137@gmail.com>
2014-09-11 13:16:05 -0700

    Added tinder features for scrolling paintings

--- client/www/scripts/controllers/artRecommend.js
+++ client/www/scripts/controllers/artRecommend.js
@@ -1,6 +1,6 @@
-angular.module('dangerousWrenchApp')
+var myApp = angular.module('dangerousWrenchApp')
 
-.controller('UserRecommend', function ($scope) {
+myApp.controller('UserRecommend', function ($scope,$ionicSideMenuDelegate) {
  
     $scope.items = [
     { id: 1, album: 'Lady and the Ermin', artist: 'Leonardo Da Vinci', image:"http://upload.wikimedia.org/wikipedia/commons/e/ed/Dama_z_gronostajem.jpg"},
@@ -13,6 +13,21 @@ angular.module('dangerousWrenchApp')
     { id: 8, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"}
 
   ]
+
+    $scope.selectedIndex = [];
   
+  $scope.itemClicked = function ($index) {
+      console.log($index);
+      $scope.selectedIndex.push($index)
+      console.log(selectedIndex.indexOf($index)!==-1)
+
+    }
+
+  $scope.toggleLeft = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+      };
+    
     
 })
+
+
