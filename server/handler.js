dc45b638cc2a44fa60222ed8e88b4b16d28bd4d3
wykhuh <wykhuh@users.noreply.github.com>
2014-09-12 11:15:16 -0700

    (feat/search) Fixed the regex for search.

--- server/handler.js
+++ server/handler.js
@@ -144,7 +144,7 @@ module.exports = function(app) {
   })
 
 
-  // Fetches the items that the user likes
+  // Fetches all items that the user likes
   app.post('/generateUserLikes', function(req, res) {
     //may have to change names, etc., based on db format
     //'like' here = edge between usernode and artwork node
@@ -158,23 +158,21 @@ module.exports = function(app) {
     })
   });
 
-  // Searched title and artist name for a keyword
+  // Searched  painting's title, artist, and medium for a keyword
   app.post('/KeywordSearch', function(req, res) {
     var searchterms = req.body.searchterms;
     var searchterms = searchterms.split(' ');
-    // var propertyKeys = [title, dates, image, name, type, artist, value]
     var query = [];
-    // query.push('MATCH (n:Work)-[:HAS_FEATURE]-(a:Feature) WHERE ');
     query.push('MATCH (n:Work) WHERE ');
 
     console.log('SEARCHTERMS=', searchterms);
     for (var i = 0; i < searchterms.length; i++) {
-      console.log('searchterm', searchterms[i])
-      // for (var k = 0; k < propertyKeys.length; k++)
-      // query.push('(n.title =~ "(?i).*'+ searchterms[i] +'.*" OR n.image =~ ".*'+ searchterms[i] +'.*" OR n.artist =~ ".*'+ searchterms[i] +'.*" OR (a.type = "TIMELINE" AND a.value =~ ".*'+ searchterms[i] +'.*") OR (a.type = "TYPE" AND a.value =~ ".*'+ searchterms[i] +'.*") OR (a.type = "FORM" AND a.value =~ ".*'+ searchterms[i] +'.*") OR (a.type = "SCHOOL" AND a.value =~ ".*'+ searchterms[i] +'.*") OR (a.type = "TECHNIQUE" AND a.value =~ ".*'+ searchterms[i] +'.*") OR (a.type = "DATE" AND a.value =~ ".*'+ searchterms[i] +'.*"))');  
-      query.push('(n.title =~ "(?i).*'+ searchterms[i] +
-        '.*" OR n.artist =~ ".*'+ searchterms[i] +
-        '.*")');  
+
+      query.push(
+        '(n.title =~ ".*\\b'+ searchterms[i] + '\\b.*"' +
+        ' OR n.medium =~ ".*\\b'+ searchterms[i] + '\\b.*"' +
+        ' OR n.artist =~ ".*\\b'+ searchterms[i] +'\\b.*" )'
+      );  
 
       if (i < searchterms.length - 1) {
         query.push(' AND ');
@@ -192,6 +190,33 @@ module.exports = function(app) {
       res.end(searchResult);
     })
   });
+
+  // when user clicks like, add like relationship between user and painting
+  app.post('/like', function(req, res){
+
+    var params = { url: req.body.imageUrl, username: req.body.username };
+
+    db.query('MATCH (n:User {username: ({username}) }),(b:Work {url: ({url}) })\nCREATE (n)-[:LIKES {rating:1}]->(b)', params, function(err){
+
+      if (err) console.log(err);
+      console.log('like created!');
+      // console.log(params);
+      res.end();
+      console.log(res.end())
+    })
+  })
+
+
+  
+};rr, data) {
+
+      if (err) console.log(err);
+      var searchResult = utils.makeData(data, 'n');
+      searchResult = JSON.stringify(searchResult);
+
+      res.end(searchResult);
+    })
+  })
 
   // when user clicks like, incretment the like
   app.post('/like', function(req, res){
