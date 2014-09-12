1cdaec03c05a71315bf5046c350feed3cc12e36b
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-09-11 18:00:40 -0700

    enabled pagination for search results

--- client/www/scripts/controllers/searchResultsController.js
+++ client/www/scripts/controllers/searchResultsController.js
@@ -2,19 +2,36 @@ angular.module('dangerousWrenchApp')
 .controller('SearchResultsCtrl', function ($scope, $location, $rootScope, KeywordSearch) {
     $scope.searchterms;
     $scope.artData = {};
+    $scope.currentPage = 1;
+    $scope.numPerPage = 10;
+    $scope.totalItems = 00;
+    $scope.currentPageArtData = [];
+    $scope.allArtData = [];
 
     $scope.search = function() {
       $location.search('q', $scope.searchterms);
       KeywordSearch.search($scope.searchterms)
         .then(function (response) {
-          $scope.artData = response.data;
+          $scope.allArtData = response.data;
+          $scope.totalItems = $scope.allArtData.length;
+          $scope.pageChanged()
         }, function (error) {
           console.log(error);
         })
     };
+
     var q = $location.search().q;
     if (q != null) {
       $scope.searchterms = q;
       $scope.search();
     };
+
+    // Enables pagination
+    $scope.pageChanged = function() {
+      var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+      , end = begin + $scope.numPerPage;
+
+      $scope.currentPageArtData = $scope.allArtData.slice(begin, end);
+    };
+
 })
