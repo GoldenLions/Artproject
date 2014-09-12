ca8158965221734557709f992cd391bb61359681
Chun Hao Wang <chunhao137@gmail.com>
2014-09-11 19:54:24 -0700

    Added more functionality to recommendation

--- client/www/scripts/controllers/artRecommend.js
+++ client/www/scripts/controllers/artRecommend.js
@@ -3,7 +3,7 @@ var myApp = angular.module('dangerousWrenchApp')
 myApp.controller('UserRecommend', function ($scope,$ionicSideMenuDelegate) {
  
     $scope.items = [
-    { id: 1, album: 'Lady and the Ermin', artist: 'Leonardo Da Vinci', image:"http://upload.wikimedia.org/wikipedia/commons/e/ed/Dama_z_gronostajem.jpg"},
+    { id: 1, album: 'Lady and the Ermin', artist: 'Leonardo Da Vinci', image:"http://upload.wikimedia.org/wikipedia/commons/e/ed/Dama_z_gronostajem.jpg",like:0},
     { id: 2, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
     { id: 3, album: 'Bacchus and Ariadne', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Tizian_048_%28colors%29.jpg/640px-Tizian_048_%28colors%29.jpg"},
     { id: 4, album: 'Calling of Sain Matthew', artist: 'Caravaggio', image:"http://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg/640px-Caravaggio%2C_Michelangelo_Merisi_da_-_The_Calling_of_Saint_Matthew_-_1599-1600_%28hi_res%29.jpg"},
@@ -14,14 +14,16 @@ myApp.controller('UserRecommend', function ($scope,$ionicSideMenuDelegate) {
 
   ]
 
-    $scope.selectedIndex = [];
-  
-  $scope.itemClicked = function ($index) {
-      console.log($index);
-      $scope.selectedIndex.push($index)
-      console.log(selectedIndex.indexOf($index)!==-1)
 
+
+ $scope.selectedIndex = [];
+  $scope.itemClicked = function ($index) {
+    if($scope.selectedIndex.indexOf($index)===-1){
+      $scope.selectedIndex.push($index)
+      $scope.items[$index].like =true; 
+      console.log($scope.items[$index].image)
     }
+}
 
   $scope.toggleLeft = function() {
         $ionicSideMenuDelegate.toggleLeft();
