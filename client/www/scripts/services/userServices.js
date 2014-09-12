79bdae9baab279a057c95b1e15d6c24074e4cd69
wykhuh <wykhuh@users.noreply.github.com>
2014-09-11 22:27:22 -0700

    (feat/Recommendations) Hooked up reccomendations to database)

--- client/www/scripts/services/userServices.js
+++ client/www/scripts/services/userServices.js
@@ -40,7 +40,7 @@ angular.module('dangerousWrenchApp')
         var username = JSON.stringify({username: username});
         return $http({
           method: 'POST',
-          url: '/generateRecommendations', 
+          url: '/generateUserRecommendations', 
           data: username 
         }) 
       },
