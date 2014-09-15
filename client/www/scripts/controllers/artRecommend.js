d62e6aeded0481c1fa16745428c5085f6ac8dd0e
wykhuh <wykhuh@users.noreply.github.com>
2014-09-14 19:14:16 -0700

    (feat/recommend) Add timestamp to seen relationship

--- client/www/scripts/controllers/artRecommend.js
+++ client/www/scripts/controllers/artRecommend.js
@@ -2,7 +2,7 @@ var myApp = angular.module('dangerousWrenchApp')
 
 myApp.controller('UserRecommend', function ($scope,$ionicSideMenuDelegate, likeButton, userServices) {
 
-  var results = userServices.generateArtistRecommendations('demo');
+  var results = userServices.generateArtistRecommendations(userServices.userName);
     
   results.then(function(response) {
     $scope.items =response.data;
